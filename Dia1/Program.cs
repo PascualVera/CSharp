4fb64b2d204d7b04df58c078af9c41d4dbc714f4
PascualVera <pascualvera.994@gmail.com>
2022-09-13 14:42:20 +0200

    first commit

---
+++ Dia1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Tomar Nombre por consola
+Console.WriteLine("Escribe tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hello " + nombre);
+//Obtener hora actual
+string time = DateTime.Now.ToString("h:mm:ss tt");
+Console.WriteLine("The current time is {0}", time);
---
+++ Dia2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+//Ejercicio 1
+string name = ReadLine();
+string surname = ReadLine();
+string age = ReadLine();
+
+Console.WriteLine($"My name is {name} {surname} and I'm {age} y/o");
+//Ejercicio 2
+
+//Coche
+int puertas;
+int ruedas;
+string marca;
+bool itvVigente;
+
+//Mesa
+float peso;
+float largo;
+string material;
+string color;
+
+//Ejercico 3
+
+int number = 5;
+char letter = 'a';
+
+WriteLine(number >= 18);
+WriteLine(letter.GetType() == typeof(char));
+WriteLine(number >= 18 && letter.GetType() == typeof(char));
+WriteLine(number >= 18 || letter.GetType() == typeof(char));
